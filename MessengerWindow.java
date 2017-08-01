442bcd8e4b0b8aaa9a255ae769c8af565a6d594b
lengotom <lengotom@gmail.com>
2017-08-01 14:51:29 +0200

    deleted a lot of things

--- MessengerWindow.java
+++ MessengerWindow.java
@@ -12,34 +12,9 @@ import javafx.stage.Stage;
 public class MessengerWindow extends Application{
 	@Override
 	public void start(Stage stage){
-		GridPane root = new GridPane();
-		Scene scene = new Scene(root, 300, 200);
-
-		TextField tf = new TextField("Graphics the Hard Way!");
-		Button btn = new Button("Print Message");
-		btn.setOnAction(new EventHandler<ActionEvent>(){
-			@Override
-			public void handle(ActionEvent event){
-				System.out.println(tf.getText());
-			}
-		});
-// test input bla bla bla here
-		root.setAlignment(Pos.CENTER);
-		root.setVgap(20);;
-		
-		root.add(tf,  0,  0);;
-		root.add(btn,  0,  1);
-		
-		root.setHalignment(btn,  HPos.CENTER);
-// test input again here	
-		stage.setTitle("Messanger Window");
-		stage.setScene(scene);
-		stage.show();
 	}
 	
-	public static void main(String [] args){
-		launch(args);
-	}
+
 	
 	
 }
